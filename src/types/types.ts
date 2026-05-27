337a05bb5c58083ebac1d495de705e5d9e57a9d7
Luke Tyson <lukeool76@gmail.com>
2026-05-27 18:05:54 +0100

    task: create robot types

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,20 @@ export enum RobotMovement {
   right = 'R',
   forward = 'F',
 }
+
+export enum Orientation {
+  north = 'N',
+  east = 'E',
+  south = 'S',
+  west = 'W',
+}
+export interface GridPosition {
+  x: number;
+  y: number;
+}
+
+export interface Robot {
+  position: GridPosition;
+  orientation: Orientation;
+  isLost: boolean;
+}
