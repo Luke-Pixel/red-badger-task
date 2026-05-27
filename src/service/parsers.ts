8c76d15f034dc9851caad3525194a0ade5a1acec
Luke Tyson <lukeool76@gmail.com>
2026-05-27 19:28:23 +0100

    task: implement robot line parser

--- src/service/parsers.ts
+++ src/service/parsers.ts
@@ -1,4 +1,4 @@
-import { GridPosition } from '../types/types.js';
+import { GridPosition, Orientation, Robot } from '../types/types.js';
 
 export function parseGridInputLine(input: string, inputLine: number): GridPosition {
   const isValid = input.match(/^\s*(\d+)\s+(\d+)\s*$/);
@@ -12,16 +12,33 @@ export function parseGridInputLine(input: string, inputLine: number): GridPositi
   const height = parseInt(isValid[2]);
 
   if (width > 50 || width < 1) {
-    throw new Error(
-      `Line: ${inputLine}, Invalid Width: Valid width range is 1 - 50. Width = ${width}`
-    );
+    throw new Error(`Line: ${inputLine}, Invalid X: Valid width range is 1 - 50. Width = ${width}`);
   }
 
   if (height > 50 || height < 1) {
     throw new Error(
-      `Line: ${inputLine}, Invalid Height: Valid height range is 1 - 50. Height = ${height}`
+      `Line: ${inputLine}, Invalid Y: Valid height range is 1 - 50. Height = ${height}`
     );
   }
 
   return { x: width, y: height };
+}
+
+export function robotPositionParser(input: string, inputLine: number): Robot {
+  const isValid = input.match(/^\s*(\d+)\s+(\d+)\s+([NESW])\s*$/);
+
+  if (!isValid) {
+    throw new Error(
+      `Line: ${inputLine}, Invalid intital robot position, expected (X Y Orientation(N,E,S,W))`
+    );
+  }
+
+  return {
+    position: {
+      x: parseInt(isValid[1]),
+      y: parseInt(isValid[2]),
+    },
+    orientation: isValid[3] as Orientation,
+    isLost: false,
+  };
 }
